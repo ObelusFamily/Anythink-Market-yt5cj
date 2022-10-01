b75c8d1e9f3e159d7ea29d9558a2233ba046e818
Joey <19317273+JWW127@users.noreply.github.com>
2022-10-01 00:11:02 -0500

    ItemList changes

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -7,13 +7,14 @@ const ItemList = (props) => {
     return <div className="py-4">Loading...</div>;
   }
 
-  if (props.items.length === 0) {
+  if (props.items.length === 0 && props.title.length > 2) {
     return (
       <div className="py-4 no-items">
-        No items are here... yet.
         <div id="empty">No items found for {props.title}.</div>
       </div>
     );
+  } else if (props.items.length === 0) {
+    return <div className="py-4 no-items">No items are here... yet.</div>;
   }
 
   return (
